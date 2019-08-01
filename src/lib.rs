cb2a24bfa4730fee4788d06f87d91377b4a04984
Clément Audebert <clement.audebert@earthcube.eu>
2019-08-01 10:50:36 +0200

    Mix

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,14 @@ extern crate vst;
 use vst::plugin::{Info, Plugin, Category};
 use vst::buffer::AudioBuffer;
 
-struct BitCruster{bit_reduction: f32, sampling_rate: f32}
+struct BitCruster{bit_reduction: f32, sampling_rate: f32, mix: f32}
 
 impl Default for BitCruster{
     fn default() -> BitCruster {
         BitCruster {
             bit_reduction: 1.0,
-            sampling_rate: 1.0
+            sampling_rate: 1.0,
+            mix: 1.0
         }
     }
 }
@@ -22,7 +23,7 @@ impl Plugin for BitCruster {
             unique_id: 010010,
             inputs: 2, // stereo in
             outputs: 2, // stereo out
-            parameters: 2, // num_parameters
+            parameters: 3, // num_parameters
             category: Category::Effect,
             ..Default::default()
         }
@@ -31,6 +32,7 @@ impl Plugin for BitCruster {
     match index {
         0 => self.bit_reduction, 
         1 => self.sampling_rate,
+        2 => self.mix,
         _ => 0.0,
         }
     }
@@ -39,6 +41,7 @@ impl Plugin for BitCruster {
     match index {
         0 => self.bit_reduction = value.max(0.01),
         1 => self.sampling_rate = value.max(0.01),
+        2 => self.mix = value,
         _ => (),
         }
     }
@@ -47,13 +50,14 @@ impl Plugin for BitCruster {
     match index {
         0 => "Bit Reduction".to_string(),
         1 => "Sampling Rate".to_string(),
+        2 => "Mix".to_string(),
         _ => "".to_string(),
         }
     }
 
     fn get_parameter_label(&self, index: i32) -> String {
     match index {
-        0 => format!("{:.1} bits",(self.bit_reduction*14.0) + 2.0 ),
+        0 => format!("{:.1} bits",(self.bit_reduction*30.0) + 2.0 ),
         1 => "%".to_string(),
         _ => "".to_string(),
         }
@@ -61,13 +65,13 @@ impl Plugin for BitCruster {
     fn process(&mut self,  buffer: &mut AudioBuffer<f32>) {
 
     let (inputs, outputs) = buffer.split();
-    let bits = (self.bit_reduction * 14.0) + 2.0 ; // set to 2 - 16  bits range
+    let bits = (self.bit_reduction * 30.0) + 2.0 ; // set to 2 - 16  bits range
     let max_value = (bits * bits) - 1.0; // pow(bits, 2) is for losers
     
     for (input_buffer, output_buffer) in inputs.into_iter().zip(outputs.into_iter()) {
         for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
 
-            *output_sample = (((input_sample + 1.0) * (max_value/2.0)).floor() / max_value) - 1.0 ;
+            *output_sample = self.mix*(2.0*(((input_sample + 1.0) * (max_value/2.0)).floor() / max_value) - 1.0) + (1.0-self.mix)* input_sample ;
                 
             }
         }
